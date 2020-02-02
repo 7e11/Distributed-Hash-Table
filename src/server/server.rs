4743e0eb3f8c7d3fc7a5145e8e140d8937e11eb4
Evan Hruskar <hruskar.evan@gmail.com>
2020-02-02 15:01:11 -0500

    Added yaml settings

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,4 +1,4 @@
-use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::net::{TcpListener, TcpStream, SocketAddr, ToSocketAddrs};
 use std::collections::{VecDeque, HashMap};
 use std::sync::Mutex;
 use serde_json::{from_reader, to_writer};
@@ -9,9 +9,31 @@ use cse403_distributed_hash_table::protocol::BarrierCommand::{AllReady, NotAllRe
 use serde::Deserialize;
 use std::thread;
 use std::time::Duration;
+use std::path::Path;
+use std::convert::{TryFrom, TryInto};
+use std::str::FromStr;
 
 
 fn main() {
+
+    // Get config settings
+    // See: https://github.com/mehcode/config-rs/blob/master/examples/simple/src/main.rs
+    let mut config = config::Config::default();
+    // Add in server_settings.yaml
+    config.merge(config::File::from(Path::new("./settings/server_settings.yaml")))
+        .expect("Failed to merge server_settings.yaml");
+    // This will fail if we add more settings TODO: make more expendable.
+    let mut settings: HashMap<String, Vec<String>> = config.try_into().expect("Failed to parse settings");
+    // Need to remove in order to take ownership of it.
+    let node_ips = settings.remove("node_ips").expect("Failed to parse ips");
+
+    println!("Starting barrier");
+    barrier(node_ips);
+    println!("Starting application listener");
+//    start()
+}
+
+fn barrier(node_ips: Vec<String>) {
     // Distributed Barrier (My idea)
     // When coming online, go into "listen" mode and send a message to all IP's.
     // Wait for an acknowledgement from everyone (including yourself)
@@ -23,7 +45,9 @@ fn main() {
         .expect("Unable to bind listener");
 
     // Spawn the broadcast thread.
-    let handle = thread::spawn(barrier_broadcast);
+    let handle = thread::spawn(move || {
+        barrier_broadcast(node_ips);
+    });
 
     for res_stream in listener.incoming() {
         match res_stream {
@@ -39,21 +63,18 @@ fn main() {
             },
         }
     }
-
     handle.join().expect("broadcast thread not stopped");
-
-    println!("Starting application listener")
-//    start()
 }
 
-fn barrier_broadcast() {
+fn barrier_broadcast(node_ips: Vec<String>) {
     // Attempt to open a TCP connection with everyone.
-    let node_ips = [[127, 0, 0, 1], [192, 0, 2, 1]];
+//    let node_ips = [[127, 0, 0, 1], [192, 0, 2, 1]];
+    // east, west, central
     let mut open_streams = Vec::new();
 
     for ip in &node_ips {
         let stream_res = TcpStream::connect_timeout(
-            &SocketAddr::from((*ip, 40480)),
+            &SocketAddr::from_str((ip.to_owned() + ":40480").as_ref()).unwrap(), //TODO Very gross
             Duration::from_secs(5));
         match stream_res {
             Ok(stream) => open_streams.push(stream),
@@ -61,7 +82,7 @@ fn barrier_broadcast() {
                 println!("Failed broadcast to {:?}: {}", ip, e);
                 // write NotAllReady to the ones that succeeded and return
                 for mut stream in open_streams {
-                    to_writer(&mut stream, &NotAllReady);
+                    to_writer(&mut stream, &NotAllReady).expect("Failed to send NotAllReady");
                 }
                 return;
             },
@@ -71,7 +92,7 @@ fn barrier_broadcast() {
 
     // If everything worked, then write AllReady to all of them.
     for mut stream in open_streams {
-        to_writer(&mut stream, &AllReady);
+        to_writer(&mut stream, &AllReady).expect("Failed to send AllReady");
     }
 }
 
